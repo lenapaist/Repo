8dba8d524cb26751c7caa21978d3de2a4b9af1a8
Elena Shilova <lena.paist2000@gmail.com>
2021-11-25 18:27:12 +0300

    выполнила урок 1

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const title = "Hello World";
+const screens = "Hello World";
+const screenPrice = "Hello World";
+const rollback = "Hello World";
+const fullPrice = "Hello World";
+const adaptive = "Hello World";
 
+alert("Hello World");
+console.log("Hi!");
