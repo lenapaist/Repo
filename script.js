0b609c7f4dc848d07e33b340ae7318b6c1d64c68
Elena Shilova <lena.paist2000@gmail.com>
2021-11-26 18:04:48 +0300

    выполнила урок 2

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const title = "Hello World";
-const screens = "Hello World";
-const screenPrice = "Hello World";
-const rollback = "Hello World";
-const fullPrice = "Hello World";
-const adaptive = "Hello World";
+let title = "Project",
+    screens = "Простые, Сложные, Интерактивные",
+    screenPrice = 1000,
+    rollback = 10,
+    fullPrice = 1000000,
+    adaptive = true;
 
 alert("Hello World");
 console.log("Hi!");
+
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log(
+    `Стоимость верстки экранов ${screenPrice} рублей/долларов/гривен/юани \nСтоимость разработки сайта ${fullPrice} рублей/долларов/гривен/юани`,
+);
+console.log(screens.toLowerCase().split());
+console.log(fullPrice * (rollback / 100));
